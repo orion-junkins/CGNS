40e49164bb7eb00dc77a646afb252570ba61cd5d
orion-junkins <orionjunkins@gmail.com>
2026-06-02 10:19:01 -0700

    image viewer added

---
+++ viewer/bistroNight/data.js
@@ -0,0 +1,41 @@
+var data = {
+    "imageBoxes": [    
+        {
+            "title": "",
+            "elements": [
+                {
+                    "image": "PT_4spp.png",
+                    "title": "PT (4 spp)",
+                },
+                {
+                    "image": "BL_M1_R30.png",
+                    "title": "Baseline (M=1, R=30)",
+                },
+                {
+                    "image": "BL_M1_R50.png",
+                    "title": "Baseline (M=1, R=50)",
+                },
+                {
+                    "image": "BL_M1_R100.png",
+                    "title": "Baseline (M=1, R=100)",
+                },
+                {
+                    "image": "Ours_fast_M1_R30_K32.png",
+                    "title": "Ours Fast (M=1, R=30, K=32)",
+                },
+                {
+                    "image": "Ours_balanced_M1_R50_K32.png",
+                    "title": "Ours Balanced (M=1, R=50, K=32)",
+                },
+                {
+                    "image": "Ours_conservative_M1_R100_K32.png",
+                    "title": "Ours Conservative (M=1, R=100, K=32)",
+                },
+                {
+                    "image": "Reference.png",
+                    "title": "Reference",
+                },
+            ]
+        },    
+    ]
+}
